da31c21ca139f6c091516db77812955c1a44178d
mlewissmith <mlewissmith@users.noreply.github.com>
2023-02-15 13:52:27 +0000

    wip mvn_build.sh

--- mvn_build.sh
+++ mvn_build.sh
@@ -1,8 +1,36 @@
 #!/bin/bash
-set -eu
-myPath=$(realpath $0)
-myDir=$(dirname ${myPath})
+set -eux
+myName=$(realpath $0)
+myDir=$(dirname ${myName})
 buildDir="${myDir}/guacamole-client"
 
-cd ${buildDir}
-mvn -Dmaven.javadoc.skip=true -DskipTests=true package
+_prep() {
+    cd ${myDir}
+    git submodule init
+    git submodule update
+}
+
+_build() {
+    cd ${myDir}
+    mvn -f ${buildDir} -Dmaven.javadoc.skip=true -DskipTests=true package
+}
+
+_install() {
+    cd ${myDir}
+    cp -v ${buildDir}/guacamole/target/guacamole*.war ${myDir}
+    for tdir in $(find ${buildDir}/extensions -type d -name "target")
+    do
+        for jarfile in $(find $tdir -name "*.jar")
+        do
+            cp -v ${jarfile} $myDir
+        done
+    done
+}
+
+case ${1:-all} in
+    prep) _prep ;;
+    build) _build ;;
+    install) _install ;;
+    all) _prep ; _build ; _install ;;
+    *) echo "[ERROR]" >&2 ; exit 1 ;;
+esac
