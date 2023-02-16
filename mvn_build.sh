0019a80f56238379a2a9dd000ff8ff8fc5ec9d8b
mlewissmith <mlewissmith@users.noreply.github.com>
2023-02-16 13:04:58 +0000

    wip mvn_build

--- mvn_build.sh
+++ mvn_build.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
-set -eux
+set -eu
 myName=$(realpath $0)
-myDir=$(dirname ${myName})
-buildDir="${myDir}/guacamole-client"
+myBasename=$(basename ${myName})
+workDir=$(dirname ${myName})
+buildDir="${workDir}/guacamole-client"
+
+
+usage() {
+    cat<<EOF
+Usage: ${myBasename} [ prep | build | install | all | clean | help ]
+    prep    - prepare build environment
+    build   - execute buildscript (maven)
+    install - drop build jarfiles in ${workDir}
+    all     - (default) prep+build+install
+    clean   - clean *all* untracked files 
+              (including '${workDir}/*')
+EOF
+}
 
 _prep() {
-    cd ${myDir}
+    cd ${workDir}
     git submodule update --init
     git submodule foreach git clean -xdf
     git submodule foreach git reset --hard
@@ -13,19 +27,24 @@ _prep() {
 }
 
 _build() {
-    cd ${myDir}
+    cd ${workDir}
     mvn -f ${buildDir} -Dmaven.javadoc.skip=true -DskipTests=true package
 }
 
 _install() {
-    cd ${myDir}
+    cd ${workDir}
     for tdir in $(find ${buildDir}/{guacamole,extensions/guacamole-auth-*} -type d -name "target")
     do
         for jarfile in ${tdir}/guacamole-*.{war,jar}
         do
-            [[ -e $jarfile ]] && cp -v -t ${myDir} ${jarfile}
+            [[ -e $jarfile ]] && cp -v -t ${workDir} ${jarfile}
         done
     done
+}
+
+_clean() {
+    git clean -xdff
+    git submodule foreach git clean -xdff
 }
 
 case ${1:-all} in
@@ -33,5 +52,7 @@ case ${1:-all} in
     build) _build ;;
     install) _install ;;
     all) _prep ; _build ; _install ;;
-    *) echo "[ERROR]" >&2 ; exit 1 ;;
+    clean) _clean ;;
+    help) usage ; exit 0 ;;
+    *) usage ; exit 1 ;;
 esac
